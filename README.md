42bed06d9b916c3dafc158e932c0258f38894317
Hristo Buyukliev <Hristo@tbibuy.bg>
2022-09-02 21:37:45 +0300

    Simplify solution

--- README.md
+++ README.md
@@ -6,14 +6,7 @@
 ```
 cd python
 pip install -r requirements.txt
-python solution.py --input-images <input bin file>
-```
-### 1.1 optional arguments:
-```
---threshold-red # by default 200
---correction-red # by default 150
---min-size
---max-size # these define what size eyes we are looking for. By default, we're only looking at eyes of size 1
+python main.py
 ```
 
 ## How the solution works:
@@ -27,6 +20,3 @@ I apply the first one as a 2d convolutional filter along the image. If it's acti
 1. I am only using the red channel, so I'm only working with it. This saves time in the conversion between numpy and `StridedImage`. 
 2. I am using scipy's convolve2d instead of rolling out my own convolution. It's using numpy, which is using Fortran, so it's much faster than what Python would be. It's also vectorized. 
 3. I'm only applying one mask, instead of four, and then detecting the four eyes only for the pixels which are a match. 
-
-## Notes:
-1. I am saving the cleared images as the same format bin files, which takes a lot of time. I might have also made a mistake in the format of the bin files somewhere.
--- python/solution.py
+++ python/solution.py
@@ -6,7 +6,6 @@ from utils.image import (
     to_image,
     to_stride_image,
 )
-from solution import compute_solution
 from einops import rearrange
 import numpy as np
 from scipy.signal import convolve2d
@@ -85,22 +84,22 @@ def create_f4_mask(size):
     mask[size*3:size*4,size*3:size*4] = 1
     return mask
 
-def correct_one_eye(image, threshold_image, size, x, y, correction_red=150):
+def correct_one_eye(image, threshold_image, size, x, y):
     if (threshold_image[x-size*4:x+1, y-size*4:y+1]*f1_masks[size]).sum() == 17*size**2:
-        image[x-size*4:x+1, y-size*4:y+1] = image[x-size*4:x+1, y-size*4:y+1] - f1_masks[size]*correction_red
+        image[x-size*4:x+1, y-size*4:y+1] = image[x-size*4:x+1, y-size*4:y+1] - f1_masks[size]*150
         
     if (threshold_image[x-size*4:x+1, y-size*4:y+1]*f2_masks[size]).sum() == 2*size**2:
-        image[x-size*4:x+1, y-size*4:y+1] = image[x-size*4:x+1, y-size*4:y+1] - f2_masks[size]*correction_red
+        image[x-size*4:x+1, y-size*4:y+1] = image[x-size*4:x+1, y-size*4:y+1] - f2_masks[size]*150
 
     if (threshold_image[x-size*4:x+1, y-size*4:y+1]*f3_masks[size]).sum() == 2*size**2:
-        image[x-size*4:x+1, y-size*4:y+1] = image[x-size*4:x+1, y-size*4:y+1] - f3_masks[size]*correction_red
+        image[x-size*4:x+1, y-size*4:y+1] = image[x-size*4:x+1, y-size*4:y+1] - f3_masks[size]*150
 
     if (threshold_image[x-size*4:x+1, y-size*4:y+1]*f4_masks[size]).sum() == 4*size**2:
-        image[x-size*4:x+1, y-size*4:y+1] = image[x-size*4:x+1, y-size*4:y+1] - f4_masks[size]*correction_red
+        image[x-size*4:x+1, y-size*4:y+1] = image[x-size*4:x+1, y-size*4:y+1] - f4_masks[size]*150
     return image
 
-def remove_eyes(image_in, threshold_red=200, correction_red=150):
-    threshold_image = (image_in >= threshold_red).astype('int')
+def remove_eyes(image_in):
+    threshold_image = (image_in >= 200).astype('int')
 
     # go through masks in descending order:
     for size in sorted(f1_masks)[::-1]:
@@ -109,7 +108,7 @@ def remove_eyes(image_in, threshold_red=200, correction_red=150):
         detected = convolve2d(threshold_image, mask) >= (17*size**2)
         if detected.sum() > 0:
             for x, y in zip(*np.where(detected)):
-                corrected_image = correct_one_eye(corrected_image, threshold_image, size, x, y, correction_red)
+                corrected_image = correct_one_eye(corrected_image, threshold_image, size, x, y)
     return corrected_image
 
 def unparse_pixel(rgba: Tuple[int]) -> int:
@@ -129,36 +128,19 @@ def dump_images(images, output_file):
             wfile.write(' '.join(pixels) + '\n')
     del ft
 
-def clear_images(args):
+def compute_solution(input_images):
     ft = FunctionTracer("compute_solution", "seconds")
-
-    image_type: fp.ImageType = fp.ImageType.StrideImageType
-    input_images, test_images = fp.generate_io_data(args.input_images, args.input_images, image_type)
     cleared_images = []
     for image_in in input_images:
         np_image_in = convert_to_numpy(image_in)
-        corrected_red_channel = remove_eyes(np_image_in, args.threshold_red, args.correction_red)
-        old_pixels = image_in.pixels_red
+        corrected_red_channel = remove_eyes(np_image_in)
         image_in.pixels_red = list(corrected_red_channel.flatten())
         cleared_images.append(image_in)
     del ft
     return cleared_images
 
+f1_masks = {size: create_f1_mask(size) for size in range(1, 2)}
+f2_masks = {size: create_f2_mask(size) for size in range(1, 2)}
+f3_masks = {size: create_f3_mask(size) for size in range(1, 2)}
+f4_masks = {size: create_f4_mask(size) for size in range(1, 2)}
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--min-size', type=int, default=1, help='minimum size of eyes to search for')
-    parser.add_argument('--max-size', type=int, default=1, help='maximum size of eyes to search for')
-    parser.add_argument('--input-images', type=str, help='input image files, must be .bin')
-    parser.add_argument('--output-file', type=str, default='output.bin',  
-                        help='where do you want me to dump cleared results?')
-    parser.add_argument('--threshold_red', type=int, default=200)
-    parser.add_argument('--correction_red', type=int, default=150)
-    args = parser.parse_args()
-    
-    f1_masks = {size: create_f1_mask(size) for size in range(args.min_size, args.max_size+1)}
-    f2_masks = {size: create_f2_mask(size) for size in range(args.min_size, args.max_size+1)}
-    f3_masks = {size: create_f3_mask(size) for size in range(args.min_size, args.max_size+1)}
-    f4_masks = {size: create_f4_mask(size) for size in range(args.min_size, args.max_size+1)}
-    cleared_images = clear_images(args)
-    dump_images(cleared_images, args.output_file)
